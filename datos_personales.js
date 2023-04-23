df090e77d54fdbddcc9c25f8c659fd50a184f268
Rogger Flores <rog7p54@gmail.com>
2023-04-22 21:04:07 -0500

    nuevo proyecto

---
+++ datos_personales.js
@@ -0,0 +1,18 @@
+function redirectToGitHub() {
+    window.location.href = 'https://github.com/usuario';
+}
+function redirectToGitHub2() {
+    window.location.href = 'https://github.com/j01an';
+}
+function redirectToGitHub3() {
+    window.location.href = 'https://github.com';
+}
+function redirectToGitHub4() {
+    window.location.href = 'https://www.youtube.com/';
+}
+function redirectToGitHub5() {
+    window.location.href = 'https://www.esan.edu.pe/';
+}
+function redirectToGitHub6() {
+    window.location.href = 'https://pa.uesan.edu.pe/';
+}
